c28555e308a90511a1a88ee1d2426814a730efef
Manish <manish.das@sprout-technology.com>
2011-04-05 13:30:50 +0545

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LunchNLearn::Application.config.session_store :cookie_store, :key => '_lunch_n_learn_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LunchNLearn::Application.config.session_store :active_record_store
