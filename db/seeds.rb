e829e763cabab3e682f21d08af4237680214f669
Manish <manish.das@sprout-technology.com>
2011-04-08 17:03:44 +0545

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+[
+  {:email => "manish@sprout.com", :password => "secret", :username => "Manish"},
+  {:email => "prasvin@sprout.com", :password => "secret", :username => "Prasvin"},
+  {:email => "dipesh@sprout.com", :password => "secret", :username => "Dipesh"},
+  ].each do |attrs|
+    User.find_or_create_by_username(attrs)
+  end
+
+  [
+    {:user_id => 13, :title => "HAML", :description => "Haml (HTML Abstraction Markup Language)", :duration => 50},
+    {:user_id => 14, :title => "SASS", :description => " Sass is a Cascading Style Sheets (CSS) metalanguage", :duration => 30},
+    {:user_id => 15, :title => "MongoDb", :description => "MongoDB is an open source, document-oriented database", :duration => 60},
+    ].each do |attrs|
+      PresentationTopic.find_or_create_by_title(attrs)
+    end
